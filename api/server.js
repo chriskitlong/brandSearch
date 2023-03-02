0d6369e06090c5deba05d72af4fcbea0f5e4b5de
chriskitlong <chriskitlong@gmail.com>
2023-03-03 02:01:35 +0800

    change port

--- api/server.js
+++ api/server.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../ui/build/index.html'))
 })
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
